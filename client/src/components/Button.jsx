0022d3be47490c7045f68ae5ed90c24fee3a1f68
Its-utsav <utsavdhimmar987@gmail.com>
2025-07-30 08:44:25 +0530

    feat: complete login page

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,11 +1,12 @@
 /**
  *
- * @param {{label:string,className:string}} param0
+ * @param {{label:string,className:string} & import("react").ButtonHTMLAttributes<HtmlHTMLAttributes>} props
  * @returns
  */
-export default function Button({ label, className = "" }) {
+export default function Button(props) {
+  const { label, className = "", ...rest } = props;
   return (
-    <button type="button" className={`btn ${className}`}>
+    <button type="button" className={`btn ${className}`} {...rest}>
       {label}
     </button>
   );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,62 @@
+import { useState } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import { useRouter } from "../context/Router.context";
+import authService from "../services/auth.services";
+import { useAuth } from "../context/User.context";
 
 export default function LoginPage() {
+  const { navigate } = useRouter();
+  const { data, login } = useAuth();
+  console.log("authdata value", data);
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await authService
+        .login(userData)
+        .then((data) => login({ _id: data._id, username: data.username }))
+        .then(() => navigate("/"))
+        .catch((reason) => setMessage(reason.message));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <form>
-        <Input label="email" type="email" required autoComplete="email" />
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="email"
+          type="email"
+          required
+          autoComplete="email"
+          value={userData.email}
+          onChange={(e) =>
+            setUserData((prevData) => ({ ...prevData, email: e.target.value }))
+          }
+        />
         <Input
           label="password"
           type="password"
           required
           autoComplete="password"
+          value={userData.password}
+          onChange={(e) =>
+            setUserData((prevData) => ({
+              ...prevData,
+              password: e.target.value,
+            }))
+          }
         />
-        <Button label="Login !!" className="btn-primary" />
+
+        <Button label="Login !!" className="btn-primary" type="submit" />
+        {message && <p>{message}</p>}
       </form>
     </div>
   );
